8739a62fbc5bd5986801eecb391148d42599f90d
samanta chaparro <samanta.chaparro87@gmail.com>
2023-04-14 18:51:36 -0300

    proyecto1

---
+++ src/proyecto1udi/Proyecto1UDI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1udi;
+import java.util.Scanner;
+/**
+ *
+ * @author SUPCOMERCIO
+ */
+public class Proyecto1UDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nombre");
+        String nom = sc.nextLine();
+        System.out.println("Ingrese apellido");
+        String app = sc.nextLine();
+       
+        
+    }
+    
+}
